045062de8c46ab3759fbec1d16ff8722f1e902eb
veronikahar <veronika.har@hotmail.com>
2020-11-14 13:41:38 -0600

    Add delete job confirm modal.

---
+++ src/components/DelJobModal.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button, Modal} from "react-bootstrap";
+
+const DelJobModal = (props) => {
+
+  return (
+    <>
+      <Modal show={props.modalShow} onHide={props.toggleModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete job</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to delete this job?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={(e) => {
+            props.deleteJob(props.job); props.toggleModal()
+          }}>
+            Delete
+          </Button>
+          <Button variant="secondary" onClick={(e) => { props.toggleModal()}}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default DelJobModal;
--- src/components/WishList.js
+++ src/components/WishList.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import JobCard from "./JobCard";
+import DelJobModal from "./DelJobModal";
 
 const WishList = (props) => {
+    const [delModalShow, setDelModalShow] = useState(false),
+        [job, setJob]=useState();
+
+    const toggleModal = () => setDelModalShow(!delModalShow);
+    const handleDelClick= jobToDel => {
+        toggleModal();
+        console.log(jobToDel);
+        setJob(jobToDel);
+    }
+    
   return (
     <div className="wishlist">
+        <DelJobModal 
+            job={job}
+            modalShow={delModalShow}
+            toggleModal={toggleModal}
+            deleteJob={props.deleteJob} />
         {props.jobs.map((job) => {
-          return <JobCard key={job.timestamp} job={job} deleteJob={props.deleteJob} />;
+          return <JobCard key={job.timestamp} job={job} handleDelClick={handleDelClick} />;
         })}
     </div>
   );
